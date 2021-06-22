48a15e04288e24585af1d0d82154eebfc382eb3c
Tatiana S <tatyana-selyuk@yandex.com>
2021-06-22 22:45:45 +0300

    Added Webpack;

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const config = {
+    entry: {
+        main: './src/index.js'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+        publicPath: ''
+    },
+    mode: 'development',
+    devServer: {
+        open: true,
+        host: "localhost",
+        contentBase: path.resolve(__dirname, './dist'),
+        compress: true,
+        port: 8080,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            inject: 'body',
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/i,
+                loader: "babel-loader",
+                exclude: '/node_modules/',
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|woff(2)?|eot|ttf|otf)$/,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, {
+                    loader: 'css-loader',
+                    options: { importLoaders: 1 }
+                },
+                    'postcss-loader'],
+            },
+        ],
+    },
+};
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = "production";
+
+        config.plugins.push(new MiniCssExtractPlugin());
+    } else {
+        config.mode = "development";
+    }
+    return config;
+};
